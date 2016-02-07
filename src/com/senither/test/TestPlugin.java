1e8eb01de555c2a31935408c2b850377e1a014e3
Alexis Tan <alexis@sen-dev.com>
2016-02-07 15:47:44 +0100

    Removing unused imports

--- src/com/senither/test/TestPlugin.java
+++ src/com/senither/test/TestPlugin.java
@@ -15,7 +15,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
